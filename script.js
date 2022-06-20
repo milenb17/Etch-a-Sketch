3c7e73f265c58e1ac94fbd287ba87a1a4dcef0c9
milenb17 <milenb17@gmail.com>
2022-06-20 19:00:20 -0400

    Add draw and resize functionality to grid

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
 let container = document.querySelector("#container");
-// create row of columns
-// Outer loop creates column 
-for (let i = 0; i < 16; i++) {
-    const column = document.createElement('div');
-    column.classList.add('column');
-    container.appendChild(column);
-    // inner loop creates 16 boxes in each column
-    for (let k = 0; k < 16; k++) {
-        const div = document.createElement('box');
-        div.classList.add('box');
-        column.appendChild(div);
+makeGrid(16);
+makeBoxListeners();
+
+
+
+
+// button which prompts to change size
+let button = document.querySelector('button');
+let size;
+button.addEventListener('click', function() {
+    do {
+        size = prompt("Enter a grid size less than 100");
+    } while (size >= 100);
+    size = parseInt(size);
+    container.innerHTML = '';
+    makeGrid(size);
+    makeBoxListeners();
+});
+
+
+
+function makeGrid(n) {
+    // Outer loop creates column
+    for (let i = 0; i < n; i++) {
+        const column = document.createElement('div');
+        column.classList.add('column');
+        container.appendChild(column);
+        // inner loop creates 16 boxes in each column
+        for (let k = 0; k < n; k++) {
+            const div = document.createElement('box');
+            div.classList.add('box');
+            column.appendChild(div);
+        }
     }
+}
+
+// add event listener to draw on etch a sketch
+function makeBoxListeners() {
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {box.addEventListener('mouseover', function (e) {
+        console.log(e.target);
+        e.target.classList.add('color')})});
 }
