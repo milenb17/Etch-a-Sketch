00c88d95f8306eaedc5004d99e753fd4f7ab62b4
milenb17 <milenb17@gmail.com>
2022-06-22 21:12:43 -0400

    Fix clear button functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let mode = 'black';
 let container = document.querySelector("#container");
 makeGrid(16);
 makeBoxListeners();
@@ -6,18 +7,37 @@ makeBoxListeners();
 
 
 // button which prompts to change size
-let button = document.querySelector('button');
+let resizeButton = document.querySelector('#resize');
 let size;
-button.addEventListener('click', function() {
+resizeButton.addEventListener('click', function() {
     do {
         size = prompt("Enter a grid size less than 100");
     } while (size >= 100);
+    console.log(size);
     size = parseInt(size);
-    container.innerHTML = '';
-    makeGrid(size);
-    makeBoxListeners();
+    console.log(size);
+    if (!isNaN(size)) {
+        container.innerHTML = '';
+        makeGrid(size);
+        makeBoxListeners();
+    }
+    
 });
 
+//button which clears grid
+let clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', function() {
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {box.style.cssText = 'background-color: white;'});
+});
+
+// rainbow mode button
+let rainbowButton = document.querySelector('#rainbow');
+rainbowButton.addEventListener('click', function(){mode = 'color'});
+
+// black mode button
+let blackButton = document.querySelector('#black');
+blackButton.addEventListener('click', function(){mode = 'black'});
 
 
 function makeGrid(n) {
@@ -26,7 +46,7 @@ function makeGrid(n) {
         const column = document.createElement('div');
         column.classList.add('column');
         container.appendChild(column);
-        // inner loop creates 16 boxes in each column
+        // inner loop creates n boxes in each column
         for (let k = 0; k < n; k++) {
             const div = document.createElement('box');
             div.classList.add('box');
@@ -37,8 +57,26 @@ function makeGrid(n) {
 
 // add event listener to draw on etch a sketch
 function makeBoxListeners() {
+    let mouseIsDown = false
+    document.addEventListener('mousedown', function(){mouseIsDown = true});
+    document.addEventListener('mouseup', function(){mouseIsDown = false});
     let boxes = document.querySelectorAll(".box");
     boxes.forEach((box) => {box.addEventListener('mouseover', function (e) {
-        console.log(e.target);
-        e.target.classList.add('color')})});
+        if(mouseIsDown) {
+            if (mode === 'black') {
+                console.log(e.target);
+                e.target.style.cssText = 'background-color: black;';
+            }
+            else {
+                e.target.style.cssText = 'background-color: ' + randColor();
+            }
+        }
+    })});
+}
+
+function randColor() {
+    rVal = Math.floor(Math.random() * 256);
+    gVal = Math.floor(Math.random() * 256);
+    bVal = Math.floor(Math.random() * 256);
+    return `rgb(${rVal},${gVal},${bVal})`;
 }
